a0735ccbb15842ab30526f5fb91d22909b18efcc
furnapso <luke.boland95@gmail.com>
2024-05-16 16:25:26 +1000

    add animation and move profile pic

--- pages/index.vue
+++ pages/index.vue
@@ -2,37 +2,42 @@
 </script>
 
 <template>
-  <div class="h-screen w-1/2 flex flex-col justify-center items-center text-slate-100 mx-auto">
+  <div
+      class="h-screen w-1/2 flex flex-col justify-center items-center text-slate-100 mx-auto animate__animated animate__fadeInDown">
     <div class="p-5 rounded border-slate-700">
-      <h1 class="text-5xl font-mono pb-1">Hi, I'm Luke</h1>
       <div>
-        <div>I'm a software engineer in Brisbane, Australia.</div><br/>
-        <div>Senior Software Engineer at Flight Centre Travel Group | Co-Founder and Lead Engineer at <a href="https://groupt.au/">Groupt</a></div>
-        <div class="border-b border-dotted my-5"/>
-        <div class="flex">
-          <div class="text-sm mt-4">A dynamic and results-oriented Senior Software Engineer with a proven track record
-            of driving
-            innovation and delivering high-quality solutions in fast-paced environments. I excel in leveraging
-            cutting-edge
-            technologies to streamline processes and enhance overall efficiency.<br/><br/>
+        <img src="@/public/profile_pic_cropped.png" alt="Profile" class="max-h-32 ml-2 rounded-2xl float-right"/>
+        <h1 class="text-5xl font-mono pb-1">Hi, I'm Luke</h1>
+        <div>I'm a software engineer in Brisbane, Australia.</div>
+        <br/>
+        <div>Senior Software Engineer at Flight Centre Travel Group | Co-Founder and Lead Engineer at <a
+            href="https://groupt.au/">Groupt</a></div>
+      </div>
+      <div class="border-b border-dotted my-5"/>
+      <div class="flex">
+        <div class="text-sm mt-4">A dynamic and results-oriented Senior Software Engineer with a proven track record
+          of driving
+          innovation and delivering high-quality solutions in fast-paced environments. I excel in leveraging
+          cutting-edge
+          technologies to streamline processes and enhance overall efficiency.<br/><br/>
 
-            Currently, as a Senior Software Engineer at Flight Centre Travel Group, I utilize my expertise in Java,
-            Spring and Vue.js to build robust, scalable enterprise software
-            to meet the ever-changing needs of the travel industry.<br/><br/>
+          Currently, as a Senior Software Engineer at Flight Centre Travel Group, I utilize my expertise in Java,
+          Spring and Vue.js to build robust, scalable enterprise software
+          to meet the ever-changing needs of the travel industry.<br/><br/>
 
-            I am passionate about software engineering as a craft and take pride in the work that I do. As a self-taught
-            software engineer, I am a zealous reader and learner and am always on the lookout for ways to upgrade my skills.<br/><br/>
-          </div>
-          <img src="@/public/profile_pic.jpg" alt="Profile" class="max-h-56 ml-2 rounded-2xl"/>
+          I am passionate about software engineering as a craft and take pride in the work that I do. As a
+          self-taught
+          software engineer, I am a zealous reader and learner and am always on the lookout for ways to upgrade my
+          skills.<br/><br/>
         </div>
-        <div class="border-b border-dotted my-5"/>
-        <Skills />
-        <div class="border-b border-dotted my-5"/>
-        <div class="flex">
-          <a href="https://www.linkedin.com/in/luke-boland-36110a5b/" class="mr-3 transition hover:text-slate-500">LinkedIn</a>
-          <a href="https://github.com/furnapso/" class="mx-3">Github</a>
-          <a href="https://github.com/furnapso/" class="mx-3">hello@lukeboland.au</a>
-        </div>
+      </div>
+      <div class="border-b border-dotted my-5"/>
+      <Skills/>
+      <div class="border-b border-dotted my-5"/>
+      <div class="flex">
+        <a href="https://www.linkedin.com/in/luke-boland-36110a5b/" class="mr-3 transition hover:text-slate-500">LinkedIn</a>
+        <a href="https://github.com/furnapso/" class="mx-3">Github</a>
+        <a href="https://github.com/furnapso/" class="mx-3">hello@lukeboland.au</a>
       </div>
     </div>
   </div>
