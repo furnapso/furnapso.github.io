9d6bc3b9c6b967288f22a1c9d630758ffb2834a3
furnapso <luke.boland95@gmail.com>
2024-05-16 13:07:42 +1000

    add google fonts and font awesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss", "@vesp/nuxt-fontawesome"],
+  googleFonts: {
+    families: {
+      Roboto: true,
+      FiraCode: true,
+      "Radio Canada": true,
+    }
+  },
+  fontawesome: {
+    icons: {
+      brands: ['vuejs', 'js', 'html5', 'java', 'python', 'css3-alt', 'react']
+    }
+  }
 })
